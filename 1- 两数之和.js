f44fa6be1dae8a50a148d1de6e2d9ae4ab308dc9
zhangXin <2668000581@qq.com>
2020-05-11 10:31:42 +0800

    两数之和

--- 1- 两数之和.js
+++ 1- 两数之和.js
@@ -1,5 +1,4 @@
 // 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
-
 // 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 
 // 示例:
@@ -7,7 +6,6 @@
 
 // 因为 nums[0] + nums[1] = 2 + 7 = 9
 // 所以返回 [0, 1]
-
 
 // 暴力破解法
 var twoSum = function(nums, target) {
@@ -21,3 +19,15 @@ var twoSum = function(nums, target) {
     }
     return -1
 };
+
+// 进化-Map
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+      let dif = target-nums[i]
+      if (map.has(dif)) {
+        return [map.get(dif), i]
+      }
+      map.set(nums[i], i);
+    }
+  };
