798e5dccc5e4bb8c7c0bc0eb778b0a704315045d
zhangXin <2668000581@qq.com>
2020-09-27 19:53:49 +0800

    feat

--- 简易/9.回文数.js
+++ 简易/9.回文数.js
@@ -9,8 +9,41 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
 
+// 一
+// var isPalindrome = function(x) {
+//     if(x<0) return false;
+//     let originN = x; 
+//     let newN = 0;
+//     while(x>0){
+//         let tmp = x%10;
+//         newN = newN*10 + tmp;
+//         x = (x-tmp)/10;
+//     }
+//     return originN === newN;
+// };
+
+
+
+
+
+
+
+
+// 二
+// var isPalindrome = function(x) {
+//     if(x<0) {
+//         return false;
+//     } else if(String.prototype.split.call(x,'').reverse().join('') != x) {
+//         return false 
+//     }
+//     return true
+// };
+
+
+// 三
+var isPalindrome = function(x) {
+    
 };
 // @lc code=end
 
