1f93c443b3361a81e4e061047f0da8a7fa596ee5
zhangXin <2668000581@qq.com>
2020-10-21 17:28:05 +0800

    LT

---
+++ 简易/107.二叉树的层次遍历-ii.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=107 lang=javascript
+ *
+ * [107] 二叉树的层次遍历 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+
+};
+// @lc code=end
+
--- 简易/27.移除元素.js
+++ 简易/27.移除元素.js
@@ -11,7 +11,15 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-
+  let i = 0;
+  while(i<nums.length){
+      if(nums[i] == val) {
+          nums.splice(i,1)
+      } else {
+          i++
+      }
+  }
+  return nums.length
 };
 // @lc code=end
 
