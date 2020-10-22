90ab5ef5e3c30f1964eb9dca28ee06facc633ce1
zhangXin <2668000581@qq.com>
2020-10-22 11:23:44 +0800

    er

--- 简易/35.搜索插入位置.js
+++ 简易/35.搜索插入位置.js
@@ -10,9 +10,30 @@
  * @param {number} target
  * @return {number}
  */
+
+ // 暴利破解
 var searchInsert = function(nums, target) {
-    
+        let idx = nums.indexOf(target);
+        if(idx >= 0){
+            return idx;
+        }
+        for(let i = 0; i < nums.length; i++){
+            if(nums[i] > target){
+                return i;
+            }
+        }
+        return nums.length;
 };
+
+
+
+
+
+
+
+
+
+
 // @lc code=end
 
 // 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
